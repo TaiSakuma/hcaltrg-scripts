8dbad7bc5e909db5b7c948972d8642a9a6bfc60a
Tai Sakuma <tai.sakuma@gmail.com>
2016-07-28 16:17:42 +0200

    add a new scribbler HFPreRecHit

--- Scribbler.py
+++ Scribbler.py
@@ -47,6 +47,57 @@ class MET(object):
         self.handlePFMETs = None
 
 ##__________________________________________________________________||
+class HFPreRecHit(object):
+    def begin(self, event):
+        self.hfrechit_ieta = [ ]
+        self.hfrechit_iphi = [ ]
+        self.hfrechit_QIE10_index = [ ]
+        self.hfrechit_QIE10_energy = [ ]
+        self._attach_to_event(event)
+
+        self.handleHFPreRecHit = Handle("edm::SortedCollection<HFPreRecHit,edm::StrictWeakOrdering<HFPreRecHit> >")
+        # SortedCollection: https://github.com/cms-sw/cmssw/blob/CMSSW_8_1_X/DataFormats/Common/interface/SortedCollection.h
+        # HFPreRecHit: https://github.com/cms-sw/cmssw/blob/CMSSW_8_1_X/DataFormats/HcalRecHit/interface/HFPreRecHit.h
+
+    def _attach_to_event(self, event):
+        event.hfrechit_ieta = self.hfrechit_ieta
+        event.hfrechit_iphi = self.hfrechit_iphi
+        event.hfrechit_QIE10_index = self.hfrechit_QIE10_index
+        event.hfrechit_QIE10_energy = self.hfrechit_QIE10_energy
+
+    def event(self, event):
+        self._attach_to_event(event)
+
+        edm_event = event.edm_event
+
+        edm_event.getByLabel('hfprereco', self.handleHFPreRecHit)
+        hfPreRecoHits = self.handleHFPreRecHit.product()
+
+        self.hfrechit_ieta[:] = [h.id().ieta() for h in hfPreRecoHits]*2
+        self.hfrechit_iphi[:] = [h.id().iphi() for h in hfPreRecoHits]*2
+        self.hfrechit_QIE10_index[:] = [0]*len(hfPreRecoHits) + [1]*len(hfPreRecoHits)
+        self.hfrechit_QIE10_energy[:] = [h.getHFQIE10Info(i).energy() for i in (0, 1) for h in hfPreRecoHits] + [h.getHFQIE10Info(1).energy() for h in hfPreRecoHits]
+
+        # print self.hfrechit_QIE10_0_energy
+        return
+        for rechit in hfPreRecoHits:
+            print rechit.id().ieta(),
+            print rechit.id().iphi(),
+            print rechit.getHFQIE10Info(0).charge(),
+            print rechit.getHFQIE10Info(0).energy(),
+            print rechit.getHFQIE10Info(0).timeRising(),
+            print rechit.getHFQIE10Info(0).timeFalling(),
+            print rechit.getHFQIE10Info(0).nRaw(),
+            print rechit.getHFQIE10Info(0).soi(),
+            print
+
+        # print hfPreRecoHit.getHFQIE10Info(0)
+        # print hfPreRecoHit.getHFQIE10Info(1)
+
+    def end(self):
+        self.handleHFPreRecHit = None
+
+##__________________________________________________________________||
 class Scratch(object):
     def begin(self, event):
         self._attach_to_event(event)
