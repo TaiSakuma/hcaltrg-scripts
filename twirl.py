3e507bce1c5fe761df1150a9fecd8502065d4711
Tai Sakuma <tai.sakuma@gmail.com>
2016-07-29 12:54:38 +0200

    update twirl.py to add tbl_custom_counts_test.txt

--- twirl.py
+++ twirl.py
@@ -50,20 +50,19 @@ def main():
         dict(branchNames = ('lumi', ), binnings = (echo, )),
         dict(branchNames = ('eventId', ), binnings = (echo, )),
         dict(branchNames = ('pfMet', ), binnings = (Round(10, 0), )),
-        dict(
-            branchNames = ('hfrechit_QIE10_energy', ),
-            binnings = (Round(0.1, 0), ),
-            indices = ('(*)', ),
-            outColumnNames = ('energy', ),
-
-        ),
-        dict(
-            branchNames = ('hfrechit_ieta', 'hfrechit_iphi', 'hfrechit_QIE10_index', 'hfrechit_QIE10_energy'),
-            binnings = (echo, echo, echo, Round(0.1, 0)),
-            indices = ('(*)', '\\1', '\\1', '\\1'),
-            outColumnNames = ('ieta', 'iphi', 'idxQIE10', 'energy'),
-
-        ),
+        # dict(
+        #     branchNames = ('hfrechit_QIE10_energy', ),
+        #     binnings = (Round(0.1, 0), ),
+        #     indices = ('(*)', ),
+        #     outColumnNames = ('energy', ),
+        #
+        # ),
+        # dict(
+        #     branchNames = ('hfrechit_ieta', 'hfrechit_iphi', 'hfrechit_QIE10_index', 'hfrechit_QIE10_energy'),
+        #     binnings = (echo, echo, echo, Round(0.1, 0)),
+        #     indices = ('(*)', '\\1', '\\1', '\\1'),
+        #     outColumnNames = ('ieta', 'iphi', 'idxQIE10', 'energy'),
+        # ),
     ]
 
     # complete table configs
@@ -82,6 +81,13 @@ def main():
     )
 
     #
+    # add custom readers
+    #
+    reader_collector_pairs.extend(
+        build_custom_reader_collector_pairs()
+    )
+
+    #
     # configure data sets
     #
     dataset = Framework.Dataset('root3', [args.input])
@@ -94,6 +100,156 @@ def main():
         dataset = dataset,
         reader_collector_pairs = reader_collector_pairs
     )
+
+##__________________________________________________________________||
+def build_custom_reader_collector_pairs():
+    ret = [ ]
+
+    Binning = AlphaTwirl.Binning.Binning
+    Echo = AlphaTwirl.Binning.Echo
+    Round = AlphaTwirl.Binning.Round
+    RoundLog = AlphaTwirl.Binning.RoundLog
+    Combine = AlphaTwirl.Binning.Combine
+    echo = Echo(nextFunc = None)
+    tblcfg = [
+        dict(
+            branchNames = ('pfMet', ),
+            binnings = (Round(10, 0), ),
+            weight = AlphaTwirl.Counter.WeightCalculatorOne(),
+            outFilePath = os.path.join(args.outdir, 'tbl_custom_counts_test.txt'),
+            outFile = True,
+            countsClass = CustomCounts,
+            outColumnNames = ('met',),
+            indices = None,
+        ),
+    ]
+    # ret.extend([custom_build_counter_collector_pair(c) for c in tblcfg])
+
+    outColumnNames = ('ieta', 'iphi', 'idxQIE10')
+    outFilePath = os.path.join(args.outdir, 'tbl_custom_hfprefechit_energy.txt')
+    resultsCombinationMethod = CombineIntoList(keyNames = outColumnNames)
+    deliveryMethod = WriteListToFile(outFilePath)
+    collector = Collector(resultsCombinationMethod, deliveryMethod)
+    ret.append([HFPreRecHit(), collector])
+
+    return ret
+
+##__________________________________________________________________||
+import collections
+class CustomCounts(object):
+    def __init__(self):
+        self._counts = { }
+        self.branchValName = 'energy'
+        self.outColumnValName = 'energy0'
+
+    def count(self, key, w = 1, nvar = None):
+        self.addKey(key)
+        self._counts[key][self.outColumnValName] += w
+
+    def addKey(self, key):
+        if key not in self._counts:
+            self._counts[key] = collections.OrderedDict(((self.outColumnValName, 0.0), ))
+
+    def keys(self):
+        return self._counts.keys()
+
+    def valNames(self):
+        return ('n', )
+
+    def copyFrom(self, src):
+        self._counts.clear()
+        self._counts.update(src._counts)
+
+    def results(self):
+        return self._counts
+
+##__________________________________________________________________||
+from AlphaTwirl.Counter.WeightCalculatorOne import WeightCalculatorOne
+class CustomCounter(object):
+    def __init__(self, keyComposer, countMethod, nextKeyComposer = None,
+                 weightCalculator = WeightCalculatorOne()):
+        self.keyComposer = keyComposer
+        self.countMethod = countMethod
+        self.weightCalculator = weightCalculator
+        self.nextKeyComposer = nextKeyComposer
+
+    def begin(self, event):
+        self.keyComposer.begin(event)
+
+    def event(self, event):
+        keys = self.keyComposer(event)
+        weight = self.weightCalculator(event)
+        for key in keys:
+            self.countMethod.count(key, weight)
+
+    def end(self):
+        if self.nextKeyComposer is None: return
+        for key in sorted(self.countMethod.keys()):
+            nextKeys = self.nextKeyComposer(key)
+            for nextKey in nextKeys: self.countMethod.addKey(nextKey)
+ 
+    def valNames(self):
+        return self.countMethod.valNames()
+
+    def copyFrom(self, src):
+        self.countMethod.copyFrom(src.countMethod)
+
+    def results(self):
+        return self.countMethod.results()
+
+##__________________________________________________________________||
+class HFPreRecHit(object):
+    def __init__(self):
+        self.outColumnValName = ('ieta', 'iphi', 'idxQIE10')
+        self.outColumnValName = 'energy'
+
+        self._counts = { }
+
+    def begin(self, event): pass
+
+    def event(self, event):
+        for c in zip(
+            event.hfrechit_ieta,
+            event.hfrechit_iphi,
+            event.hfrechit_QIE10_index,
+            event.hfrechit_QIE10_energy,
+            ):
+            key = c[0:3]
+            val = c[3]
+            if key not in self._counts:
+                self._counts[key] = collections.OrderedDict(((self.outColumnValName, 0.0), ))
+            self._counts[key][self.outColumnValName] += val
+
+    def end(self): pass
+
+    def valNames(self):
+        return (self.outColumnValName, )
+
+    def copyFrom(self, src):
+        self._counts.clear()
+        self._counts.update(src._counts)
+
+    def results(self):
+        return self._counts
+
+##__________________________________________________________________||
+from AlphaTwirl.Counter import Counter, GenericKeyComposerB, NextKeyComposer
+from AlphaTwirl.CombineIntoList import CombineIntoList
+from AlphaTwirl.WriteListToFile import WriteListToFile
+from AlphaTwirl.EventReader import Collector
+def custom_build_counter_collector_pair(tblcfg):
+    keyComposer = GenericKeyComposerB(tblcfg['branchNames'], tblcfg['binnings'], tblcfg['indices'])
+    nextKeyComposer = NextKeyComposer(tblcfg['binnings'])
+    counter = CustomCounter(
+        keyComposer = keyComposer,
+        countMethod = tblcfg['countsClass'](),
+        nextKeyComposer = nextKeyComposer,
+        weightCalculator = tblcfg['weight']
+    )
+    resultsCombinationMethod = CombineIntoList(keyNames = tblcfg['outColumnNames'])
+    deliveryMethod = WriteListToFile(tblcfg['outFilePath']) if tblcfg['outFile'] else None
+    collector = Collector(resultsCombinationMethod, deliveryMethod)
+    return counter, collector
 
 ##__________________________________________________________________||
 if __name__ == '__main__':
