cb3d26b3b13e1b41e6643a687eea5636442bad33
Tai Sakuma <tai.sakuma@gmail.com>
2016-08-12 10:00:24 +0200

    update table config

--- twirl.py
+++ twirl.py
@@ -56,9 +56,9 @@ def main():
         dict(keyAttrNames = ('eventId', ), binnings = (echo, )),
         dict(keyAttrNames = ('pfMet', ), binnings = (Round(10, 0), )),
         dict(keyAttrNames = ('genParticle_pdgId', ), keyIndices = ('*', ), binnings = (echoNextPlusOne, ), keyOutColumnNames = ('gen_pdg', )),
-        dict(keyAttrNames = ('genParticle_eta', ), keyIndices = ('*', ), binnings = (Round(0.1, 0), ), keyOutColumnNames = ('gen_eta', )),
-        dict(keyAttrNames = ('genParticle_pdgId', 'genParticle_eta'), keyIndices = ('(*)', '\\1'), binnings = (echoNextPlusOne, Round(0.1, 0)), keyOutColumnNames = ('gen_pdg', 'gen_eta')),
-        dict(keyAttrNames = ('genParticle_phi', ), keyIndices = ('*', ), binnings = (Round(0.0314159265*3, 0), ), keyOutColumnNames = ('gen_phi', )),
+        dict(keyAttrNames = ('genParticle_eta', ), keyIndices = ('*', ), binnings = (Round(0.2, 0), ), keyOutColumnNames = ('gen_eta', )),
+        dict(keyAttrNames = ('genParticle_pdgId', 'genParticle_eta'), keyIndices = ('(*)', '\\1'), binnings = (echoNextPlusOne, Round(0.2, 0)), keyOutColumnNames = ('gen_pdg', 'gen_eta')),
+        dict(keyAttrNames = ('genParticle_phi', ), keyIndices = ('*', ), binnings = (Round(0.0314159265*5, 0), ), keyOutColumnNames = ('gen_phi', )),
         dict(keyAttrNames = ('genParticle_energy', ), keyIndices = ('*', ), binnings = (Round(0.1, 0), ), keyOutColumnNames = ('gen_energy', )),
         dict(
             keyAttrNames = ('hfrechit_ieta', 'hfrechit_iphi', 'hfrechit_QIE10_index'),
@@ -69,8 +69,13 @@ def main():
             keyOutColumnNames = ('ieta', 'iphi', 'idxQIE10'),
             valOutColumnNames = ('energy', ),
             summaryClass = AlphaTwirl.Summary.Sum,
-            outFile = True,
         ),
+        dict(keyAttrNames = ('hfrechit_QIE10_charge', ), keyIndices = ('*', ), binnings = (Round(0.1, 0), ), keyOutColumnNames = ('QIE10_charge', )),
+        dict(keyAttrNames = ('hfrechit_QIE10_energy', ), keyIndices = ('*', ), binnings = (Round(0.1, 0), ), keyOutColumnNames = ('QIE10_energy', )),
+        dict(keyAttrNames = ('hfrechit_QIE10_timeRising', ), keyIndices = ('*', ), binnings = (Round(0.1, 0), ), keyOutColumnNames = ('QIE10_timeRising', )),
+        dict(keyAttrNames = ('hfrechit_QIE10_timeFalling', ), keyIndices = ('*', ), binnings = (Round(0.1, 0), ), keyOutColumnNames = ('QIE10_timeFalling', )),
+        dict(keyAttrNames = ('hfrechit_QIE10_nRaw', ), keyIndices = ('*', ), binnings = (Round(1, 0), ), keyOutColumnNames = ('QIE10_nRaw', )),
+        dict(keyAttrNames = ('hfrechit_QIE10_soi', ), keyIndices = ('*', ), binnings = (Round(1, 0), ), keyOutColumnNames = ('QIE10_soi', )),
     ]
 
     # complete table configs
